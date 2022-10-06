58d035b0ec051423394de2933eb8dd0dc13d114f
Benoit Roland <benoit.roland@kit.edu>
2022-10-06 18:32:58 +0200

    list samples and check files: download-indexfiles.sh

--- h4leptons/Download-Indexfiles/download-indexfiles.sh
+++ h4leptons/Download-Indexfiles/download-indexfiles.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-if [ $# != 1 ]
-then
-   echo "Specify the indexfiles to download: data2011, data2012, moca2011, moca2012 or a specific indexfile"
-   echo ""
-   echo "data 2011 indexfiles: data2011/Run2011A_DoubleMu, data2011/Run2011A_DoubleElectron"
-   echo ""
-   echo "data 2012 indexfiles: data2012/Run2012B_DoubleMuParked, data2012/Run2012C_DoubleMuParked, data2012/Run2012B_DoubleElectron, data2012/Run2012C_DoubleElectron"
-   echo ""
-   echo "moca 2011 indexfiles: moca2011/ZZTo4mu, moca2011/ZZTo4e, moca2011/ZZTo2e2mu"
-   echo "                      moca2011/TTTo2L2Nu2B, moca2011/SMHiggsToZZTo4L"
-   echo "                      moca2011/DYJetsToLL_M-10To50, moca2011/DYJetsToLL_M-50"
-   echo ""
-   echo "moca 2012 indexfiles: moca2012/ZZTo4mu, moca2012/ZZTo4e, moca2012/ZZTo2e2mu"
-   echo "                      moca2012/TTTo2L2Nu2B, moca2012/SMHiggsToZZTo4L"
-   echo "                      moca2012/DYJetsToLL_M-10to50_HT-200to400, moca2012/DYJetsToLL_M-10to50_HT-400toInf, moca2012/DYJetsToLL_M-50"
-   exit
+if [ $# != 1 ]; then
+    echo "Specify the indexfiles to download: data2011, data2012, moca2011, moca2012 or a specific indexfile"
+    echo ""
+    echo "data 2011 indexfiles: data2011/Run2011A_DoubleMu, data2011/Run2011A_DoubleElectron"
+    echo ""
+    echo "data 2012 indexfiles: data2012/Run2012B_DoubleMuParked, data2012/Run2012C_DoubleMuParked, data2012/Run2012B_DoubleElectron, data2012/Run2012C_DoubleElectron"
+    echo ""
+    echo "moca 2011 indexfiles: moca2011/ZZTo4mu, moca2011/ZZTo4e, moca2011/ZZTo2e2mu"
+    echo "                      moca2011/TTTo2L2Nu2B, moca2011/SMHiggsToZZTo4L"
+    echo "                      moca2011/DYJetsToLL_M-10To50, moca2011/DYJetsToLL_M-50"
+    echo ""
+    echo "moca 2012 indexfiles: moca2012/ZZTo4mu, moca2012/ZZTo4e, moca2012/ZZTo2e2mu"
+    echo "                      moca2012/TTTo2L2Nu2B, moca2012/SMHiggsToZZTo4L"
+    echo "                      moca2012/DYJetsToLL_M-10to50_HT-200to400, moca2012/DYJetsToLL_M-10to50_HT-400toInf, moca2012/DYJetsToLL_M-50"
+    exit
 fi
 
 sample=$1    
@@ -27,17 +26,13 @@ declare -i num_total
 declare -i num_downloaded
 
 while read -r database_sample database_index; do
-
+    
     WGET="wget --no-check-certificate -P $Indexfiles"
-
+    
     if [[ $database_sample == *$sample* ]]; then
-
+	
 	num_total=$(grep $database_sample $database | wc -l)
-	num_downloaded=0
-
-	if ! [ -z "$(ls $Indexfiles/$database_sample)" ]; then
-            num_downloaded=$(ls $Indexfiles/$database_sample/*.txt | wc -l)
-	fi	  
+        num_downloaded=$(find $Indexfiles/$database_sample -not -type d -and -not -name '.*' | wc -l) 
 
 	if [[ $num_downloaded < $num_total ]]; then
 	    echo "" 
@@ -55,5 +50,5 @@ while read -r database_sample database_index; do
 	fi
 	
     fi
-
+    
 done < "$database"
