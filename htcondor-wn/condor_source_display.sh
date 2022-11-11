7014a72d12fa7d161617d2910f7c46be1d4b0269
Benoit Roland <benoit.roland@kit.edu>
2022-11-11 12:07:15 +0100

    fixes for X11 forwarding

---
+++ htcondor-wn/condor_source_display.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [ -r /srv/.display ]; then 
+  source /srv/.display 
+  rm -f /srv/.display
+fi
---
+++ htcondor-wn/condor_xauth_wrapper.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Walk up the process tree until we find the second sshd which rewrites cmdline to "sshd: user@tty".
+# The first sshd is our parent process which does not log itself.
+SSHD_PID=$$
+SSHD_CNT=0
+while true; do
+  IFS= read -r -d '' CMDLINE </proc/${SSHD_PID}/cmdline || [[ $cmdline ]]
+  #echo "Checking ID ${SSHD_PID}, cmdline ${CMDLINE^^}"
+  SSHD_MATCHER="^SSHD: "
+  if [[ ${CMDLINE^^} =~ ${SSHD_MATCHER} ]]; then
+    # We found the sshd!
+    SSHD_CNT=$(( SSHD_CNT + 1))
+    if [ ${SSHD_CNT} -gt 1 ]; then
+      break;
+    fi
+  fi
+  SSHD_PID=$(ps -o ppid= -p ${SSHD_PID} | awk '{print $1}')
+  if [ ${SSHD_PID} -eq 1 ]; then
+    # We arrived at the INIT process, something very wrong... Let's stop and alert the user.
+    echo "Error: Could not determine sshd process, X11 forwarding will not work!"
+    echo "       Please let your admins know you got this error!"
+    exit 0
+  fi
+done
+#echo "SSHD PID is ${SSHD_PID}."
+
+# Find sshd.log, checking through fds.
+FOUND_SSHD_LOG=0
+for FD in $(ls -1 /proc/${SSHD_PID}/fd/); do
+  FILE=$(readlink -f /proc/${SSHD_PID}/fd/$FD)
+  #echo "Checking FD $FD, file is $FILE"
+  SSHD_LOG_MATCHER="sshd\.log$"
+  if [[ "${FILE}" =~ ${SSHD_LOG_MATCHER} ]]; then
+    #echo "Found ${FILE}!"
+    FOUND_SSHD_LOG=1
+    SSH_TO_JOB_DIR=$(dirname ${FILE})
+    JOB_WORKING_DIR=$(dirname ${SSH_TO_JOB_DIR})
+    break;
+  fi
+done
+
+if [ ${FOUND_SSHD_LOG} -eq 0 ]; then
+  # We could not identify sshd.log, let's stop and alert the user.
+  echo "Error: Could not determine sshd process' (PID: ${SSHD_PID}) log, X11 forwarding will not work!"
+  echo "       Please let your admins know you got this error!"
+  exit 0
+fi
+
+# Finally, if we arrive here, all is well.
+
+# This does NOT work, since env.sh is sourced as forced command, too early.
+#echo "export DISPLAY=${DISPLAY}" >> ${SSH_TO_JOB_DIR}/env.sh
+
+# Ugly hack needed with HTCondor 8.8.10 which does not yet pass through DISPLAY or TERM.
+echo "export DISPLAY=${DISPLAY}" > ${JOB_WORKING_DIR}/.display
+echo "export TERM=${TERM}" >> ${JOB_WORKING_DIR}/.display
+
+export XAUTHORITY=${JOB_WORKING_DIR}/.Xauthority
+/usr/bin/xauth "$@" </dev/stdin
