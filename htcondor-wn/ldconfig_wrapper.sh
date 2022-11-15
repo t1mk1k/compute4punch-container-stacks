def95a70a15bd07c77f9af84051322d582d53419
Benoit Roland <benoit.roland@kit.edu>
2022-11-15 14:26:24 +0100

    /scratch/ld.so.cache in wlcg-wn

--- htcondor-wn/ldconfig_wrapper.sh
+++ htcondor-wn/ldconfig_wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "executing script ldconfig_wrapper.sh" > /tmp/condor_ldconfig_wrapper.log
+echo "executing script ldconfig_wrapper.sh from htcondor-wn" > /tmp/condor_htcondor_ldconfig_wrapper.log
 /sbin/ldconfig -C /scratch/ld.so.cache "$@"
