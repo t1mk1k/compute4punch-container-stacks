7facb3c592ed0fdac8c98ac1944bf8fefa1ad15c
Benoit Roland <benoit.roland@kit.edu>
2022-11-24 11:08:45 +0100

    disable X11Forwarding

--- htcondor-wn/display.sh
+++ htcondor-wn/display.sh
@@ -3,6 +3,9 @@
 echo "executing script display.sh" > /tmp/condor_display.log
 
 if [ -r /srv/.display ]; then
+  echo "/srv/.display present" >> /tmp/condor_display.log
   source /srv/.display
   rm -f /srv/.display
+else
+  echo "/srv/.display NOT present" >> /tmp/condor_display.log
 fi
