0c6e34e02020fd362f5114b7ee730425bc93e176
Manuel Giffels <giffels@gmail.com>
2022-12-21 12:57:50 +0100

    Enable build of linc-wn again

--- build_docker_containers.sh
+++ build_docker_containers.sh
@@ -5,10 +5,6 @@ CONTAINERS=$(ls */[Dd]ockerfile)
 
 for CONTAINER in $CONTAINERS; do
   CONTAINER_NAME=${CONTAINER/\/[Dd]ockerfile/}
-  if [ "$CONTAINER_NAME" == "linc-wn" ]
-  then
-    continue
-  fi
   docker pull $CI_REGISTRY_IMAGE/$CONTAINER_NAME:latest || true
   docker build \
       --pull \
