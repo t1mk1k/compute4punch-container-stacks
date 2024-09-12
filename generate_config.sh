4f00af08b5bb70f30403df77b8750e20a42abadf
Manuel Giffels <giffels@gmail.com>
2024-09-12 12:05:06 +0200

    Exclude h4leptons, due to a bug

--- generate_config.sh
+++ generate_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CONTAINERS=$(ls */[Dd]ockerfile)
+#exclude h4leptons, since there is a bug when a Docker image contains Kernel 2.6
+CONTAINERS=$(ls */[Dd]ockerfile | grep -v h4leptons)
 
 for CONTAINER in $CONTAINERS; do
   CONTAINER_NAME=${CONTAINER/\/[Dd]ockerfile/}
